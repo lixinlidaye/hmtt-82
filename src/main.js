0b39850d6e5c4484551d4179bb473c05e7c646e2
lidaye <lidaye@qq.com>
2019-08-31 20:17:19 +0800

    初始化-整理目录

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'// 引入样式
+import './style/index.less' // 引入初始化样式
 
 Vue.config.productionTip = false
 Vue.use(ElementUI) // 全局注册elementUI组件
