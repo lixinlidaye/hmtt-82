747aceeaba868b3d06b90ae5e653f834d46653ac
lidaye <lidaye@qq.com>
2019-08-31 21:17:39 +0800

    页面布局及样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,27 @@
 <template>
-  <div>
-      login
+  <div class='login'>
+      <el-card class="login-card">
+          <div class="logo">
+              <img src="../../assets/img/logo_index.png" alt="黑马头条">
+          </div>
+          <!-- 表单组件  el-form表单容器 -->
+          <el-form style='margin-top:20px'>
+              <!-- 表单项 -->
+              <el-form-item>
+                  <el-input></el-input>
+              </el-form-item>
+              <el-form-item>
+                  <el-input style="width:250px"></el-input>
+                 <el-button style="float:right">获取验证码</el-button>
+              </el-form-item>
+              <el-form-item>
+                  <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+              </el-form-item>
+               <el-form-item>
+                 <el-button type="primary" style="width:100%">登录</el-button>
+              </el-form-item>
+          </el-form>
+      </el-card>
   </div>
 </template>
 
@@ -10,6 +31,26 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+// 用less 需要lang属性
+// scoped 只针对当前组件样式起作用
+.login {
+    background-image: url('../../assets/img/login_bg.jpg');
+    height: 100vh;
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .login-card{
+        width: 450px;
+        height: 350px;
+        .logo {
+            text-align: center;
+            img {
+                height: 45px;
+            }
 
+        }
+    }
+}
 </style>
