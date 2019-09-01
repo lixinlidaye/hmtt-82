f64912e026b0e95e5c32e6944d423f33fd7eac78
lidaye <lidaye@qq.com>
2019-09-01 22:23:18 +0800

    实现头部信息修改和头部退出系统

--- src/components/home/layout-header.vue
+++ src/components/home/layout-header.vue
@@ -6,20 +6,20 @@
             <i class="el-icon-s-unfold"></i>
             <span>爱谁谁有限公司</span>
         </el-col>
-        <el-col class="right" :span="2">
+        <el-col class="right" :span="3">
             <!-- <img class="head-img" src="../../assets/img/avatar.jpg" alt=""> -->
             <!-- 属性不给: 就相当于字符串 -->
             <img class="head-img" :src="userInfo.photo ? userInfo.photo : defaultImg" alt="">
-        <el-dropdown trigger="click">
+        <el-dropdown trigger="click" @command='handleMenuItem'>
             <!-- 匿名插槽 -->
             <span class="el-dropdown-link">
             {{userInfo.name}}<i class="el-icon-arrow-down el-icon--right"></i>
             </span>
             <!-- 具名插槽 -->
             <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item icon="el-icon-user">个人信息</el-dropdown-item>
-            <el-dropdown-item icon="el-icon-position">git地址</el-dropdown-item>
-            <el-dropdown-item icon="el-icon-more">退出</el-dropdown-item>
+            <el-dropdown-item icon="el-icon-user" command="account">个人信息</el-dropdown-item>
+            <el-dropdown-item icon="el-icon-position" command='git'>git地址</el-dropdown-item>
+            <el-dropdown-item icon="el-icon-more" command='login'>退出</el-dropdown-item>
         </el-dropdown-menu>
     </el-dropdown>
         </el-col>
@@ -28,28 +28,43 @@
 
 <script>
 export default {
+
   data () {
     return {
       userInfo: {},
-      defaultImg: require('../../assets/img/avatar.jpg')
+      defaultImg: require('../../assets/img/avatar.jpg') // 转成base64字符串
     }
   },
   methods: {
     // 获取用户数据
-    getUserInfor () {
+    getUserInfo () {
       let token = window.localStorage.getItem('user-token') // 获取token
       this.$http({
         url: '/user/profile',
-        header: { 'Authorization': `Bearer ${token}` }
-      }).then(res => {
-        this.userInfo = res.data.data
+        headers: { 'Authorization': `Bearer ${token}` }
+      }).then(result => {
+        this.userInfo = result.data.data
       })
+    },
+    //   点击菜单项触发的方法
+    handleMenuItem (command) {
+      if (command === 'account') {
+        // 账户信息
+      } else if (command === 'git') {
+        // git地址
+        window.localStorage.href = 'https://github.com/lixinlidaye/hmtt-82' // 改变当前的地址
+      } else {
+        // 退出
+        window.localStorage.clear() // 清空缓存 清除所有的缓存  只能清除自己当前项目的缓存
+        this.$router.push('/login') // 跳转到登录页
+      }
     }
   },
   created () {
-    this.getUserInfor()
+    this.getUserInfo()
   }
 }
+
 </script>
 
 <style lang='less' scoped>
