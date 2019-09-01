3e423b2737c9e81b1f168df6dc4548bf3ab65caa
lidaye <lidaye@qq.com>
2019-09-01 10:43:59 +0800

    主页模块左侧模板

---
+++ src/components/home/layout-header.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- 用el-row el-col -->
+    <el-row>
+        <el-col></el-col>
+    </el-row>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,11 +2,11 @@
 <!-- 最外层容器 -->
  <el-container>
    <!-- 左侧导航 -->
-   <el-aside>左侧</el-aside>
+   <el-aside><layout-aside></layout-aside></el-aside>
    <!-- 右侧 -->
    <el-container>
      <!-- 头部 -->
-     <el-header>头部</el-header>
+     <el-header><layout-header></layout-header></el-header>
      <!-- 主要区域 -->
      <el-main>主体</el-main>
    </el-container>
@@ -14,8 +14,21 @@
 </template>
 
 <script>
+import layoutAside from '../../components/home/layout-aside'
+import layoutHeader from '../../components/home/layout-header'
 export default {
-
+  components: {
+    'layout-aside': layoutAside,
+    'layout-header': layoutHeader
+  }
+//   methods: {
+//     handleOpen (key, keyPath) {
+//       console.log(key, keyPath)
+//       },
+//     handleClose (key, keyPath) {
+//       console.log(key, keyPath)
+//       }
+//   }
 }
 </script>
 
