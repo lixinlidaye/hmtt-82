79b9008e071fb83817fde01f5d65db3b92015151
lidaye <lidaye@qq.com>
2019-09-02 14:57:44 +0800

    axios拦截器 统一处理请求

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+// import router from './router'
+import router from './permission'
 
 import ElementUI from 'element-ui' // 框架组件
 import Component from './components' // 自定义组件
-import axios from 'axios'
+// import axios from 'axios'
+
 import 'element-ui/lib/theme-chalk/index.css'// 引入样式
 import './style/index.less' // 引入初始化样式
 
-axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
-Vue.prototype.$http = axios // 给Vue对象的原型竖向赋值 那么所有vue实例自动拥有$http
+import axios from './utils/axios.config'
+Vue.prototype.$http = axios
+// axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+// Vue.prototype.$http = axios // 给Vue对象的原型竖向赋值 那么所有vue实例自动拥有$http
 
 Vue.config.productionTip = false
 Vue.use(ElementUI) // 全局注册elementUI组件
