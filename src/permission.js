6e86d241e2faabc986e0abe32d7d86c87b17feb1
lidaye <lidaye@qq.com>
2019-09-02 11:35:43 +0800

    导航守卫

--- src/permission.js
+++ src/permission.js
@@ -1,15 +1,19 @@
-// import router from './router'
-// router.beforeEach(function (today, from, next) {
-//   if (to.path.startswith('/home')) {
-//     let token = window.localStorage.getItem('user-token')
-//     if (token) {
-//       next()
-//     } else {
-//       next('/login')
-//     }
-//   } else {
-//     next()
-//   }
-// })
+import router from './router'
 
-// export default router
+router.beforeEach(function (to, from, next) {
+  // 如果要去的地址以/home为起始 , 就说明需要判断当前是不是 登录 判断token
+
+  if (to.path.startswith('/home')) {
+    let token = window.localStorage.getItem('user-token')
+    if (token) {
+      next()
+    } else {
+      next('/login')
+    }
+  } else {
+    // 如果没有/home 可以认为你是安全的 不需要控制的页面
+    next()
+  }
+})
+
+export default router
